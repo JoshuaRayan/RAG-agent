3b3a9896539e7b57e57ab4cb4f1187cc77875740
JoshuaRayan <96614913+JoshuaRayan@users.noreply.github.com>
2025-05-09 21:11:54 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import cryptography
-conn = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='YourNewPassword123',
-    database='federal_registry'
-)
-print("Connected successfully")
